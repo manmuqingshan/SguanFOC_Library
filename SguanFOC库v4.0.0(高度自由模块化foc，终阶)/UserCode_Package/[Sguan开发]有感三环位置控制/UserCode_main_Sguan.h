b255abf104f56ae41df34457ab714007170e8456
Sguan-ZhouQing <148698322+Sguan-ZhouQing@users.noreply.github.com>
2026-08-23 16:53:40 +0800

    v4.0.0(预告...搭Simulink一样编程)

---
+++ SguanFOC库v4.0.0(高度自由模块化foc，终阶)/UserCode_Package/[Sguan开发]有感三环位置控制/UserCode_main_Sguan.h
@@ -0,0 +1,14 @@
+#if 0
+// ---------------------------二次开发Sguan---------------------------
+
+#ifndef __USERCODE_MAIN_H
+#define __USERCODE_MAIN_H
+
+
+
+
+
+#endif // USERCODE_MAIN_H
+
+// ---------------------------二次开发Sguan---------------------------
+#endif
